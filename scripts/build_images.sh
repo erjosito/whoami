31b0c52d66cdab8d1ce5533e53c8dd4d3208d706
Jose Moreno <jose.moreno@microsoft.com>
2021-02-07 13:23:06 +0100

    added build images workflow

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -6,6 +6,10 @@
 # Jose Moreno, January 2021
 ###############################################
 
+# Variables
+base_dir='.'
+repo_name='acilab'
+
 # Argument parsing (can overwrite the previously initialized variables)
 for i in "$@"
 do
@@ -16,6 +20,14 @@ do
                ;;
           -l=*|--location=*)
                location="${i#*=}"
+               shift # past argument=value
+               ;;
+          -d=*|--base-dir=*)
+               base_dir="${i#*=}"
+               shift # past argument=value
+               ;;
+          -r=*|--repo-name=*)
+               repo_name="${i#*=}"
                shift # past argument=value
                ;;
      esac
@@ -31,36 +43,37 @@ then
 fi
 
 # Build web frontend
-dir=$(ls -ald web)
+dir=$(ls -ald "${base_dir}/web")
 if [[ -n "$dir" ]]
 then
-    az acr build -t azurefriday/web:1.0 -r "$acr_name" ./web
+    az acr build -t "${repo_name}/web:1.0" -r "$acr_name" "${base_dir}/web"
 else
     echo "I cannot find the directory with the web app, are you in the right folder?"
 fi
 # Build API
-dir=$(ls -ald api)
+dir=$(ls -ald "${base_dir}/api")
 if [[ -n "$dir" ]]
 then
     # Verify the code is showing the correct version
-    version=$(grep "'version': '1.0'" ./api/sql_api.py)
+    version=$(grep "'version': '1.0'" "${base_dir}/api/sql_api.py")
     if [[ -n "$version" ]]
     then
-        az acr build -t azurefriday/api:1.0 -r "$acr_name" ./api
+        az acr build -t "${repo_name}/api:1.0" -r "$acr_name" "${base_dir}/api"
     else
         echo "Mmmmh, it looks like you have the wrong version in sql_api.py???"
-        grep "'version'" ./api/sql_api.py
+        grep "'version'" "${base_dir}/api/sql_api.py"
     fi
 else
     echo "I cannot find the directory with the API code, are you in the right folder?"
 fi
 # Build dashboard
-dir=$(ls -ald dash)
+dir=$(ls -ald "${base_dir}/dash")
 if [[ -n "$dir" ]]
 then
-    az acr build -t azurefriday/dash:1.0 -r "$acr_name" ./dash
+    az acr build -t "${repo_name}/dash:1.0" -r "$acr_name" "${base_dir}/dash"
 else
     echo "I cannot find the directory with the dashboard code, are you in the right folder?"
 fi
+
 # Verify created images
 az acr repository list -n "$acr_name" -o table
